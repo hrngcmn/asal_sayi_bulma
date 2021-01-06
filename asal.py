e76fd4202f3c0343bfa2ddf7dab1574d6bbc31a9
Harun Gocmen <68404755+hrngcmn@users.noreply.github.com>
2021-01-06 17:15:06 +0300

    .

--- asal.py
+++ asal.py
@@ -6,7 +6,7 @@ if a !=2 :# 2 sayısı asal olduğu bilinmelidir.
             #  örneğin a nın değeri 5 olsun diyelim.
         if a % i == 0:             # 5 mod 2 !=0
                                 #5 mod 3 !=0
-                                  #5 mod 4 !=0 sayma biter  (i=4 değerinde.) .
+                             #5 mod 4 !=0 sayma biter  (i=4 değerinde.) .
             break  # Eğer sıfıra eşitse for döngüsünden dan çık.
             # Girilen değer örneğin 9 olaydı  9%3 olduğunda 8. satırdaki kod çalıştığında i=3 değerinde kalırdı
 
